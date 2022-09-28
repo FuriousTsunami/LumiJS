5c8515e98981e79a965cbe40d3c3917e91819cda
FuriousTsunami <69990380+FuriousTsunami@users.noreply.github.com>
2022-09-28 07:48:44 -0400

    Moved back to Terser minify (better performance and built in compatibility)

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,9 @@ module.exports = {
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin({
-      minify: TerserPlugin.uglifyJsMinify,
+      terserOptions: {
+        
+      },
       include: /\.min\.js$/,
     })]
   },
