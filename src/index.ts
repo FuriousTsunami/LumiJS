0d2f457461062e9865a0781e4082cc567a29de6e
FuriousTsunami <sunayrkomarla@gmail.com>
2022-11-21 21:54:36 -0500

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,69 @@
 import * as PIXI from "./pixiConfig";
-import Collision from "./collision";
+import Physics from "./physics";
 
-class Scene {
-  renderer: PIXI.Renderer;
-  ticker: PIXI.Ticker;
-  collision: Collision;
-  constructor (rendererOptions: Object) {
-    this.renderer = new PIXI.Renderer(rendererOptions);
-    this.ticker = new PIXI.Ticker();
-    this.ticker.stop();
-    this.collision = new Collision(PIXI);
-  }
+class Sprite extends PIXI.Sprite {
+	constructor(texture?: PIXI.Texture) {
+		super(texture);
+	}
+
+	get globalX () {
+		return this.getGlobalPosition().x;
+	}
+
+	get globalY () {
+		return this.getGlobalPosition().y;
+	}
+
+	get centerX () {
+		return this.x + this.width / 2;
+	}
+
+	get centerY () {
+		return this.y + this.height / 2;
+	}
+
+	get halfWidth () {
+		return this.width / 2;
+	}
+
+	get halfHeight () {
+		return this.height / 2;
+	}
 }
 
-export var Lumi = {
-  Scene: Scene,
+class Body {
+	x: number;
+	y: number;
+	constructor (x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Entity {
+	body: Body;
+	sprite: Sprite;
+	constructor (body: Body, sprite: Sprite) {
+		this.body = body;
+		this.sprite = sprite;
+	}
+}
+
+class Scene {
+	renderer: PIXI.Renderer;
+	ticker: PIXI.Ticker;
+	physics: Physics;
+	constructor (rendererOptions: PIXI.IRenderOptions) {
+		this.renderer = new PIXI.Renderer(rendererOptions);
+		this.ticker = new PIXI.Ticker();
+		this.ticker.stop();
+		this.physics = new Physics();
+	}
+}
+
+export const Lumi = {
+	Sprite: Sprite,
+	Body: Body,
+	Entity: Entity,
+	Scene: Scene,
 };
