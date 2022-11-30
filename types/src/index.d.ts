e33e1a30da4b51ffa4f4d87c71df028ea503a6d3
FuriousTsunami <sunayrkomarla@gmail.com>
2022-11-30 18:51:39 -0500

    - ESLint now ignores .d.ts files - Added ESLint rules derived from @pixi/eslint-config - Changed folder path id .gitignore - Added scripts for each step of the build proccess in package.json - Updated main and module paths in package.json - Changed rollup.config.js to match ESLint rules - tsconfig.types.json now extends tsconfig.json for clarity - Removed old lumi files - Added new lumi files - Changed index.ts to match ESLint rules - Changed collision name to physics

--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -1,10 +1,32 @@
 import * as PIXI from "./pixiConfig";
+declare class Sprite extends PIXI.Sprite {
+    constructor(texture?: PIXI.Texture);
+    get globalX(): number;
+    get globalY(): number;
+    get centerX(): number;
+    get centerY(): number;
+    get halfWidth(): number;
+    get halfHeight(): number;
+}
+declare class Body {
+    x: number;
+    y: number;
+    constructor(x?: number, y?: number);
+}
+declare class Entity {
+    body: Body;
+    sprite: Sprite;
+    constructor(body: Body, sprite: Sprite);
+}
 declare class Scene {
     renderer: PIXI.Renderer;
     ticker: PIXI.Ticker;
-    constructor(rendererOptions: Object);
+    constructor(rendererOptions: PIXI.IRenderOptions);
 }
-export declare var Lumi: {
+export declare const Lumi: {
+    Sprite: typeof Sprite;
+    Body: typeof Body;
+    Entity: typeof Entity;
     Scene: typeof Scene;
 };
 export {};
