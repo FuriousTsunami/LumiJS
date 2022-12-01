bb386391eb21ae73a65eb969339a9161f1600209
FuriousTsunami <sunayrkomarla@gmail.com>
2022-11-30 22:01:16 -0500

    - Added test to ESLint ignore - Re-formatted eslintrc with better rules - Add jest config (testing) - Fixed lintfix and added jest to package.json - Re-formatted index.js - Removed ESLint warnings from physics.ts (which still has errors)

--- src/index.ts
+++ src/index.ts
@@ -2,69 +2,69 @@ import * as PIXI from "./pixiConfig";
 // import Physics from "./physics";
 
 class Sprite extends PIXI.Sprite {
-	constructor(texture?: PIXI.Texture) {
-		super(texture);
-		this.texture = texture;
-	}
+  constructor(texture?: PIXI.Texture) {
+    super(texture);
+    this.texture = texture;
+  }
 
-	get globalX() {
-		return this.getGlobalPosition().x;
-	}
+  get globalX() {
+    return this.getGlobalPosition().x;
+  }
 
-	get globalY() {
-		return this.getGlobalPosition().y;
-	}
+  get globalY() {
+    return this.getGlobalPosition().y;
+  }
 
-	get centerX() {
-		return this.x + (this.width / 2);
-	}
+  get centerX() {
+    return this.x + (this.width / 2);
+  }
 
-	get centerY() {
-		return this.y + (this.height / 2);
-	}
+  get centerY() {
+    return this.y + (this.height / 2);
+  }
 
-	get halfWidth() {
-		return this.width / 2;
-	}
+  get halfWidth() {
+    return this.width / 2;
+  }
 
-	get halfHeight() {
-		return this.height / 2;
-	}
+  get halfHeight() {
+    return this.height / 2;
+  }
 }
 
 class Body {
-	x: number;
-	y: number;
-	constructor(x = 0, y = 0) {
-		this.x = x;
-		this.y = y;
-	}
+  x: number;
+  y: number;
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 class Entity {
-	body: Body;
-	sprite: Sprite;
-	constructor(body: Body, sprite: Sprite) {
-		this.body = body;
-		this.sprite = sprite;
-	}
+  body: Body;
+  sprite: Sprite;
+  constructor(body: Body, sprite: Sprite) {
+    this.body = body;
+    this.sprite = sprite;
+  }
 }
 
 class Scene {
-	renderer: PIXI.Renderer;
-	ticker: PIXI.Ticker;
-	// physics: Physics;
-	constructor(rendererOptions: PIXI.IRenderOptions) {
-		this.renderer = new PIXI.Renderer(rendererOptions);
-		this.ticker = new PIXI.Ticker();
-		this.ticker.stop();
-		// this.physics = new Physics();
-	}
+  renderer: PIXI.Renderer;
+  ticker: PIXI.Ticker;
+  // physics: Physics;
+  constructor(rendererOptions: PIXI.IRenderOptions) {
+    this.renderer = new PIXI.Renderer(rendererOptions);
+    this.ticker = new PIXI.Ticker();
+    this.ticker.stop();
+    // this.physics = new Physics();
+  }
 }
 
 export const Lumi = {
-	Sprite,
-	Body,
-	Entity,
-	Scene,
+  Sprite,
+  Body,
+  Entity,
+  Scene,
 };
