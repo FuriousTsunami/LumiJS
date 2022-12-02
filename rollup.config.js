bd4684058e89023e369d2f7ab05df621824ced62
FuriousTsunami <sunayrkomarla@gmail.com>
2022-12-01 19:28:45 -0500

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,94 +5,94 @@ import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
 function genFileHeader(name) {
-	return [
-		`/*!`,
-		` * ${name} - obtained from LumiJS v${pkg.version}`,
-		` * Compiled ${(`${(new Date()).toUTCString().slice(0, -12)}(UTC)`)}`,
-		` *`,
-		` * Copyright 2019 Sunay Komarla`,
-		` *`,
-		` * Licensed under the Apache License, Version 2.0 (the "License");`,
-		` * you may not use this file except in compliance with the License.`,
-		` * You may obtain a copy of the License at`,
-		` *`,
-		` * http://www.apache.org/licenses/LICENSE-2.0`,
-		` *`,
-		` * Unless required by applicable law or agreed to in writing, software`,
-		` * distributed under the License is distributed on an "AS IS" BASIS,`,
-		` * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`,
-		` * See the License for the specific language governing permissions and`,
-		` * limitations under the License.`,
-		` */`,
-	].join("\n");
+  return [
+    `/*!`,
+    ` * ${name} - obtained from LumiJS v${pkg.version}`,
+    ` * Compiled ${(`${(new Date()).toUTCString().slice(0, -12)}(UTC)`)}`,
+    ` *`,
+    ` * Copyright 2019 Sunay Komarla`,
+    ` *`,
+    ` * Licensed under the Apache License, Version 2.0 (the "License");`,
+    ` * you may not use this file except in compliance with the License.`,
+    ` * You may obtain a copy of the License at`,
+    ` *`,
+    ` * http://www.apache.org/licenses/LICENSE-2.0`,
+    ` *`,
+    ` * Unless required by applicable law or agreed to in writing, software`,
+    ` * distributed under the License is distributed on an "AS IS" BASIS,`,
+    ` * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`,
+    ` * See the License for the specific language governing permissions and`,
+    ` * limitations under the License.`,
+    ` */`,
+  ].join("\n");
 }
 
 export default [
-	// UMD Bundle
-	{
-		input: "src/index.ts",
-		output: {
-			file: "dist/umd/lumi.js",
-			format: "umd",
-			name: "lumi",
-			exports: "named",
-			sourcemap: true,
-			banner: genFileHeader("lumi.js")
-		},
-		plugins: [
-			resolve(),
-			typescript(),
-			commonjs()
-		],
-	},
-	// UMD Bundle (Minified)
-	{
-		input: "src/index.ts",
-		output: {
-			file: "dist/umd/lumi.min.js",
-			format: "umd",
-			name: "lumi",
-			exports: "named",
-			sourcemap: true,
-			banner: genFileHeader("lumi.min.js")
-		},
-		plugins: [
-			resolve(),
-			typescript(),
-			commonjs(),
-			terser()
-		]
-	},
-	// ES Bundle
-	{
-		input: "src/index.ts",
-		output: {
-			file: "dist/mjs/lumi.mjs",
-			format: "es",
-			exports: "named",
-			sourcemap: true,
-			banner: genFileHeader("lumi.mjs")
-		},
-		plugins: [
-			resolve(),
-			typescript(),
-			commonjs()
-		]
-	},
-	// CJS Bundle
-	{
-		input: "src/index.ts",
-		output: {
-			file: "dist/cjs/lumi.cjs",
-			format: "cjs",
-			exports: "named",
-			sourcemap: true,
-			banner: genFileHeader("lumi.cjs")
-		},
-		plugins: [
-			resolve(),
-			typescript(),
-			commonjs()
-		]
-	}
+  // UMD Bundle
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/umd/lumi.js",
+      format: "umd",
+      name: "lumi",
+      exports: "named",
+      sourcemap: true,
+      banner: genFileHeader("lumi.js")
+    },
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs()
+    ],
+  },
+  // UMD Bundle (Minified)
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/umd/lumi.min.js",
+      format: "umd",
+      name: "lumi",
+      exports: "named",
+      sourcemap: true,
+      banner: genFileHeader("lumi.min.js")
+    },
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs(),
+      terser()
+    ]
+  },
+  // ES Bundle
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/mjs/lumi.mjs",
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+      banner: genFileHeader("lumi.mjs")
+    },
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs()
+    ]
+  },
+  // CJS Bundle
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/cjs/lumi.cjs",
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      banner: genFileHeader("lumi.cjs")
+    },
+    plugins: [
+      resolve(),
+      typescript(),
+      commonjs()
+    ]
+  }
 ];
